c1055c9f134681f2428ef7394d02cf43517528ba
Dominik Jurasz <jurasz.do@gmail.com>
2020-09-25 09:50:04 +0200

    Fixed coverage

---
+++ src/test/java/com/example/demo/controller/HelloControllerTest.java
@@ -0,0 +1,24 @@
+package com.example.demo.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class HelloControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void getHello_givenValidRequest_shouldReturnMessage() throws Exception {
+        mockMvc.perform(get("/hello"))
+                .andExpect(status().isOk());
+    }
+}
