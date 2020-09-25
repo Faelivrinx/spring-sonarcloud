1ecc2ee6b35e4c13e4d3d7e8b208ea231de6a7c1
Dominik Jurasz <jurasz.do@gmail.com>
2020-09-25 09:50:04 +0200

    Fixed coverage

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -1,13 +1,12 @@
 package com.example.demo.controller;
 
-import lombok.Getter;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
-    @GetMapping
+    @GetMapping("/hello")
     public String hello() {
         return "Hello world!";
     }
